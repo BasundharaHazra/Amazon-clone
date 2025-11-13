7303ee4bf0b26a25f1a84bd3d3bba050492222f0
Basundhara <bithu3084@gmail.com>
2025-11-13 21:16:49 +0530

    Fixed import path for cart.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,7 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
-import { cart } from "../../data/cart.js";
+import { cart } from "../data/cart.js";
 
 // import "../data/cart-class.js";
 //import "../data/car.js";
